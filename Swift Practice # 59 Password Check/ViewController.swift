a863212f9f67a83a69263bdf054962d9c44e8ea0
DogpaChen <dogpa0130@gmail.com>
2021-08-12 14:32:31 +0800

    Initial Commit

---
+++ Swift Practice # 59 Password Check/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 59 Password Check
+//
+//  Created by CHEN PEIHAO on 2021/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
